54f323be143a23d54c57d7dc8339e1391f62ed15
diallo <mamadouadama609@gmail.com>
2024-04-04 13:11:02 -0500

    initialize lab3 spring project

---
+++ src/test/java/edu/miu/cs489/ElibraryApplication/ElibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.cs489.ElibraryApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
